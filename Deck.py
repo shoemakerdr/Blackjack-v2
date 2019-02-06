e72d005350819b8fb466d38e767214f5909c757d
JR Valdez <40616631+raulvaldez4@users.noreply.github.com>
2019-02-06 11:14:08 -0600

    Add files via upload

---
+++ Deck.py
@@ -0,0 +1,28 @@
+from Card import *
+
+class Deck:
+    """Deck class that holds 6 decks of cards"""
+    def __init__(self):
+        self.cards = []
+        for deck in range(6):
+            for rank in ranks:
+                for suit in suits:
+                    self.cards.append(Card(rank, suit))
+
+    def __str__(self):
+        decks = ''
+        for card in self.cards:
+            decks += '\n' + card.__str__()
+        return decks
+
+    def shuffle_cards(self):
+        return random.shuffle(self.cards)
+
+    def deal_card(self):
+        single_card = self.cards.pop()
+        return single_card
+
+# deck = Deck()
+# print(deck.deal_card())
+# deck.shuffle_cards()
+# print(deck)
